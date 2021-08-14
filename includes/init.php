fbe81029b3577bada2ee1e8fcd4385d2d5efac55
Sharaz Shahid <bc140401486@vu.edu.pk>
2021-08-14 12:52:11 +0500

    On page without reload payment functionaly added

--- includes/init.php
+++ includes/init.php
@@ -8,11 +8,11 @@
  namespace Pdfw;
 
 class Init {
-	 /**
-	  * Store all the classes inside an array
-	  *
-	  * @return array Full list of classes
-	  */
+	/**
+	* Store all the classes inside an array
+	*
+	* @return array Full list of classes
+	*/
 	public static function get_services() {
 		return [
 			Classes\Enqueue::class,
@@ -35,6 +35,25 @@ class Init {
 
 		add_action( 'plugins_loaded', array( $this, 'init_pdfw_gateway_class' ) );
 		add_filter( 'woocommerce_payment_gateways', array( $this, 'add_pdfw_gateway_class' ) );
+		add_action( 'wc_ajax_ajax_process_checkout', array( $this, 'pdfw_ajax_process_checkout' ) );
+		add_action( 'wc_ajax_nopriv_ajax_process_checkout', array( $this, 'pdfw_ajax_process_checkout' ) );
+
+		add_action( 'wp_enqueue_scripts', array( $this, 'pfw_frontend_enqueue_scripts' ) );
+	}
+
+	
+	// Callback function for frontend styles and scripts
+	public function pfw_frontend_enqueue_scripts() {
+		wp_enqueue_style( 'pfw-style', PADDLE_WOO_URL . 'assets/css/frontend-style.css' );
+		
+		$paddle_w = new \WC_Pdfw();
+		$endpoint = is_wc_endpoint_url('order-pay') ? '?wc-ajax=ajax_process_payment' : '?wc-ajax=ajax_process_checkout';
+		wp_enqueue_script( 'paddle', 'https://cdn.paddle.com/paddle/paddle.js', array('jquery'));
+		wp_enqueue_script( 'pfw-script', PADDLE_WOO_URL . 'assets/js/frontend-script.js', ('jquery') );
+		wp_localize_script('pfw-script','Pdfw', array(
+		'process_checkout'=> home_url( '/'.$endpoint),
+		'vendor_id'=> $paddle_w->vendor_id
+	));
 	}
 
 	/**
@@ -68,5 +87,16 @@ class Init {
 	public function add_pdfw_gateway_class( $methods ) {
 		$methods[] = 'WC_Pdfw';
 		return $methods;
+	}
+
+	/**
+	 * WooCommerce Checkout ajax callback.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	function pdfw_ajax_process_checkout() {
+		WC()->checkout()->process_checkout();
+		// hit "process_payment()"
 	}
 }
